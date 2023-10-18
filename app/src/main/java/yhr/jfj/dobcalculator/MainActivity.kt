5916a70d8be13279e6636bb285188ef6194d120a
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-18 18:45:04 +0600

    Add functionality in datePicker button

--- app/src/main/java/yhr/jfj/dobcalculator/MainActivity.kt
+++ app/src/main/java/yhr/jfj/dobcalculator/MainActivity.kt
@@ -1,11 +1,36 @@
 package yhr.jfj.dobcalculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener {
+            clickedDatePicker()
+        }
+    }
+    private fun clickedDatePicker(){
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show() },
+            year,
+            month,
+            day).show()
+
+
     }
 }
