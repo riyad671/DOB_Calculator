9790d47335921f3a46a8aed7fb8eec09acafdf66
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-18 19:11:36 +0600

    Show selected date

--- app/src/main/java/yhr/jfj/dobcalculator/MainActivity.kt
+++ app/src/main/java/yhr/jfj/dobcalculator/MainActivity.kt
@@ -4,15 +4,21 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import java.util.Calendar
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var tvSelectedDate : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Button
         val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
+        // Date picker
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
 
         btnDatePicker.setOnClickListener {
             clickedDatePicker()
@@ -25,12 +31,14 @@ class MainActivity : AppCompatActivity() {
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
         DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show() },
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(this, "$selectedDayOfMonth-${selectedMonth + 1 }-$selectedYear", Toast.LENGTH_SHORT).show()
+                val selectedDate = "$selectedDayOfMonth-${selectedMonth + 1 }-$selectedYear"
+                tvSelectedDate?.text = selectedDate
+
+                                               },
             year,
             month,
             day).show()
-
-
     }
 }
