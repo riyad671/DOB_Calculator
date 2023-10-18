c9a151552732e5952c1dd6a529592282e460ceb3
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-18 20:28:16 +0600

    Show different in minute

--- app/src/main/java/yhr/jfj/dobcalculator/MainActivity.kt
+++ app/src/main/java/yhr/jfj/dobcalculator/MainActivity.kt
@@ -10,7 +10,11 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private var tvSelectedDate : TextView? = null
+    // Date picker
+    private var tvSelectedDate: TextView? = null
+
+    // Show in minute
+    private var tvAgeInMinute: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,26 +23,38 @@ class MainActivity : AppCompatActivity() {
         val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
         // Date picker
         tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        // Show in minute
+        tvAgeInMinute = findViewById(R.id.tvAgeInMinute)
 
         btnDatePicker.setOnClickListener {
             clickedDatePicker()
         }
     }
-    private fun clickedDatePicker(){
+
+     private fun clickedDatePicker() {
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(this,
+        DatePickerDialog(
+            this,
             DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
-                Toast.makeText(this, "$selectedDayOfMonth-${selectedMonth + 1 }-$selectedYear", Toast.LENGTH_SHORT).show()
-                val selectedDate = "$selectedDayOfMonth-${selectedMonth + 1 }-$selectedYear"
+                Toast.makeText(this, "$selectedDayOfMonth/${selectedMonth + 1 }/$selectedYear", Toast.LENGTH_SHORT).show()
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
                 tvSelectedDate?.text = selectedDate
-
-                                               },
+                // Date to minute
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+                val selectedDateInMinute = theDate.time / 60000
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val currentDateInMinute = currentDate.time / 60000
+                val differentInMinute = currentDateInMinute - selectedDateInMinute
+                tvAgeInMinute?.text = differentInMinute.toString()
+            },
             year,
             month,
-            day).show()
+            day
+        ).show()
     }
 }
